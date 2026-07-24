3e8a604b0ae1d179e18bd45bb7548349d25a7191
emipoliana <emilipoliana0@gmail.com>
2026-07-24 13:08:10 -0300

    Add files via upload

---
+++ Trabalho JS/script.js
@@ -0,0 +1,15 @@
+let nome = prompt("Qual é o seu nome?")
+ console.log("Olá" + nome)
+
+let cidade = prompt("Qual é o sua cidade?")
+ console.log("Olá" + cidade)
+
+
+
+
+
+ if (nome == "Natal") { 
+    alert ("Aprovado")
+ } else{
+    alert("Reprovado")
+ }
